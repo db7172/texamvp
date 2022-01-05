87916bc827ae29ca095425777c8ce18388b14c79
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-01-05 23:42:22 +0530

    initial page setup

--- src/pages/view-more/ViewMoreDetailsForWorkcation.tsx
+++ src/pages/view-more/ViewMoreDetailsForWorkcation.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+import { startCase } from "lodash";
+import { TitleBreadCrumb } from "Models";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+type ParamTypes = {
+  destinationName: string;
+  workationType: string;
+};
 
 const ViewMoreDetailsForWorkcation = () => {
+  const [slashedTableName, setSlashedTableName] = useState<
+    Array<TitleBreadCrumb>
+  >([]);
+  const { destinationName, workationType } = useParams<ParamTypes>();
+  const WORKCATION_TYPE = startCase(workationType);
+  const DESTINATION_NAME = startCase(destinationName);
+
+  useEffect(() => {
+    // setEventDetails(state);
+    setSlashedTableName([
+      {
+        name: "Home",
+        url: "/",
+      },
+      {
+        name: "Events",
+        url: "/events",
+      },
+      {
+        name: DESTINATION_NAME,
+        url: `/workcation/${DESTINATION_NAME}`,
+      },
+      {
+        name: DESTINATION_NAME,
+        url: "",
+      },
+    ]);
+  }, [DESTINATION_NAME, WORKCATION_TYPE]);
+
   return (
     <div>
       <p>ViewMoreDetailsForRetreat</p>
