48a57ac0d4ab6e7a23cef8dff181104702955f10
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-01-18 22:40:18 +0530

    miner fix and log out confirmation modal

--- src/pages/User/Dashboard/UserDashboard.tsx
+++ src/pages/User/Dashboard/UserDashboard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Col, Divider, Row } from "antd";
+import { Avatar, Button, Col, Divider, Modal, Row } from "antd";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
@@ -10,6 +10,7 @@ import UserReviewsTab from "../../../components/user/user-tabs/UserReviewsTab";
 import UserSupport from "../../../components/user/user-tabs/UserSupport";
 import { USER_DASHBOAR_TABS } from "./userData";
 import firebase from "../../../firebase";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 
 const avatarImg =
   "https://imgr.search.brave.com/JuLSZUsD98Tow_UcPp9WhSQGohn_xuKhVDZRvE9AEi4/fit/1000/1080/ce/1/aHR0cHM6Ly9jZG4y/LnZlY3RvcnN0b2Nr/LmNvbS9pLzEwMDB4/MTAwMC80OS84Ni9t/YW4tY2hhcmFjdGVy/LWZhY2UtYXZhdGFy/LWluLWdsYXNzZXMt/dmVjdG9yLTE3MDc0/OTg2LmpwZw";
@@ -18,6 +19,7 @@ const UserDashboard = () => {
   const [activeTab, setActiveTab] = useState(1);
 
   const history = useHistory();
+  const { confirm } = Modal;
 
   function handleLogout() {
     firebase.auth().signOut();
@@ -30,6 +32,22 @@ const UserDashboard = () => {
       }
     });
   }, []);
+
+  function showConfirm() {
+    confirm({
+      title: "Are you sure?",
+      icon: <ExclamationCircleOutlined />,
+      okText: "Log Out",
+      okType: "danger",
+      cancelText: "Cancel",
+      onOk() {
+        firebase.auth().signOut();
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  }
 
   return (
     <Container>
@@ -77,7 +95,7 @@ const UserDashboard = () => {
             <Divider className="tw-m-0" />
             <UserTab
               activeId={activeTab}
-              handleActiveId={handleLogout}
+              handleActiveId={showConfirm}
               {...USER_DASHBOAR_TABS.logOut}
             />
           </Row>
